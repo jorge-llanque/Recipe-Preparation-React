73ea4b2f4611e9ebdf4f3e342e21d564b363ee01
jorge <georgellanque@gmail.com>
2021-07-27 03:18:44 -0400

    Create search form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useLocation} from 'wouter'
 import './styles/Home.css'
 
 export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [path, pushLocation] = useLocation()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    pushLocation(`/search/${keyword}`)
+  }
+  
+  const handleChange = e => {
+    setKeyword(e.target.value)
+  }
+  console.log(keyword)
+
   return (
     <>
       <header className="Header" >
         <h1 className="Header__Title" >Healthy habits for a better life</h1>
         <p className="Header__Subtitle" >The pocket nutritionist app that will help you reach your health and weight goals</p>
       </header>
-      <form className="Form--Search" >
-        <input type="text" placeholder="Search a recipe, an ingredient ..." />
+      <form onSubmit={handleSubmit} className="Form--Search" >
+        <input onChange={handleChange} type="text" placeholder="Search a recipe, an ingredient ..." value={keyword} />
       </form>
     </>
   )
