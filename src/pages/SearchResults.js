216fcaa42a0ec8b4cb0cae324960c5d58bc2b3bc
jorge <georgellanque@gmail.com>
2021-07-27 03:41:48 -0400

    Add SearchResults page

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function SearchResults() {
   return (
-    <div>
-      SearchResults
-    </div>
+    <>
+      <figure className="Card__Container" >
+        <img className="Card__image" src="#"/>
+        <span className="Card__Kicker">Smoothie</span>
+        <h1 className="Card__Title" >Citrus</h1>
+        <div className="Info" >
+          <span className="Info--Relevant" >340 Kcal</span>
+        </div>
+      </figure>
+    </>
   )
 }
