39f57eea039cacac52c31c4c5ce51f04bb702e6b
jorge <georgellanque@gmail.com>
2021-07-27 04:46:21 -0400

    Add styles for search-result page

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,15 +1,54 @@
 import React from 'react'
+import './styles/SearchResults.css'
+
 
 export default function SearchResults() {
+
   return (
     <>
       <figure className="Card__Container" >
-        <img className="Card__image" src="#"/>
-        <span className="Card__Kicker">Smoothie</span>
-        <h1 className="Card__Title" >Citrus</h1>
+        
+        <img className="Card__Image" src="https://agfstorage.blob.core.windows.net/misc/FP_com/2021/04/01/Oppystrawberriesedited.jpg" />
+        
         <div className="Info" >
-          <span className="Info--Relevant" >340 Kcal</span>
+          <span className="Info__Kicker">Smoothie</span>
+          <h1 className="Info__Title" >Citrus</h1>
+          <span className="Info--Relevant" > 340 Kcal</span>
         </div>
+
+      </figure>
+      <figure className="Card__Container" >
+        
+        <img className="Card__Image" src="https://agfstorage.blob.core.windows.net/misc/FP_com/2021/04/01/Oppystrawberriesedited.jpg" />
+        
+        <div className="Info" >
+          <span className="Info__Kicker">Smoothie</span>
+          <h1 className="Info__Title" >Citrus</h1>
+          <span className="Info--Relevant" > 340 Kcal</span>
+        </div>
+
+      </figure>
+      <figure className="Card__Container" >
+        
+        <img className="Card__Image" src="https://agfstorage.blob.core.windows.net/misc/FP_com/2021/04/01/Oppystrawberriesedited.jpg" />
+        
+        <div className="Info" >
+          <span className="Info__Kicker">Smoothie</span>
+          <h1 className="Info__Title" >Citrus</h1>
+          <span className="Info--Relevant" > 340 Kcal</span>
+        </div>
+
+      </figure>
+      <figure className="Card__Container" >
+        
+        <img className="Card__Image" src="https://agfstorage.blob.core.windows.net/misc/FP_com/2021/04/01/Oppystrawberriesedited.jpg" />
+        
+        <div className="Info" >
+          <span className="Info__Kicker">Smoothie</span>
+          <h1 className="Info__Title" >Citrus</h1>
+          <span className="Info--Relevant" > 340 Kcal</span>
+        </div>
+
       </figure>
     </>
   )
