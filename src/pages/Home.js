00f93ce218f0d7061f01ec9aa3940b34a8adf5fd
jorge <georgellanque@gmail.com>
2021-07-30 03:01:38 -0400

    Change event.target.value to useRef

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import {useLocation} from 'wouter'
 import './styles/Home.css'
 
 export default function Home() {
   const [keyword, setKeyword] = useState('')
   const [path, pushLocation] = useLocation()
+  const searchInput = useRef()
 
   const handleSubmit = e => {
     e.preventDefault()
     pushLocation(`/search/${keyword}`)
   }
   
-  const handleChange = e => {
-    setKeyword(e.target.value)
+  const handleChange = () => {
+    setKeyword(searchInput.current.value)
   }
 
   return (
@@ -22,7 +23,12 @@ export default function Home() {
         <p className="Header__Subtitle" >The pocket nutritionist app that will help you reach your health and weight goals</p>
       </header>
       <form onSubmit={handleSubmit} className="Form--Search" >
-        <input onChange={handleChange} type="text" placeholder="Search a recipe, an ingredient ..." value={keyword} />
+        <input 
+          onChange={handleChange} 
+          type="text" 
+          placeholder="Search a recipe, an ingredient ..." 
+          value={keyword} 
+          ref={searchInput}/>
       </form>
     </>
   )
